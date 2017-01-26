6ce4f271ca2a284aa662246ce067219ecc7fa4e0
MARCUS HOLMBERG <maip15@student.bth.se>
2017-01-26 17:03:02 +0100

    Added short structure in Object class

--- New3DProject/Object.hpp
+++ New3DProject/Object.hpp
@@ -1,6 +1,32 @@
 #pragma once
+#include <d3d11.h>
+#include <DirectXMath.h>
+
+using namespace DirectX;
+typedef unsigned int UINT;
+
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT2 TC;
+	XMFLOAT3 Normal;
+};
 
 class Object
 {
+public:
 
+	Object();
+	~Object();
+
+	bool createBuffer(ID3D10Device* device);
+	bool render(ID3D11DeviceContext* deviceCont);
+
+protected:
+
+	UINT* indices;
+	Vertex* vertices;
+	ID3D11Buffer* VertBuff;
+
+	int nrOfVertices;
 };
