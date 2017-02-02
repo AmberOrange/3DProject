c20906d07a495d4c6076b04cfa51fc190ccb4c45
Emil <emilgrenebrant@hotmail.com>
2017-02-02 12:24:34 +0100

    Put Specular into Alpha channel

--- New3DProject/deferred_pixel.hlsl
+++ New3DProject/deferred_pixel.hlsl
@@ -1,7 +1,6 @@
 Texture2D<float4> Pos_tex:register(t0);
 Texture2D<float4> Nor_tex:register(t1);
 Texture2D<float4> Dif_tex:register(t2);
-Texture2D<float4> Spec_tex:register(t3);
 
 SamplerState sampleState : register(s0);
 
@@ -13,10 +12,12 @@ struct PS_IN
 
 float4 PS_main( PS_IN input ) : SV_TARGET0
 {
-	float3 Pos = Pos_tex.Sample(sampleState, input.Tex);
-	float3 Nor = Nor_tex.Sample(sampleState, input.Tex);
-	float3 Dif = Dif_tex.Sample(sampleState, input.Tex);
-	float3 Spec = Spec_tex.Sample(sampleState, input.Tex);
-	return float4(Dif,1.f);
+	float4 Pos = Pos_tex.Sample(sampleState, input.Tex);
+	float4 Nor = Nor_tex.Sample(sampleState, input.Tex);
+	float4 Dif = Dif_tex.Sample(sampleState, input.Tex);
+	float3 Spec = float3(Pos.w, Nor.w, Dif.w);
+
+	//return Dif;
+	return float4(Spec,1.0f);
 	//return float4(0.f,0.f,1.f,0.f);
 }
--- New3DProject/gshader_pixel.hlsl
+++ New3DProject/gshader_pixel.hlsl
@@ -16,18 +16,17 @@ struct PS_OUT
 	float4 Pos : SV_TARGET0;
 	float4 Nor : SV_TARGET1;
 	float4 Dif : SV_TARGET2;
-	float4 Spec : SV_TARGET3;
 };
 
 PS_OUT PS_main(PS_IN input)
 {
 	PS_OUT output;
 	
-	output.Pos = float4(input.WPos, 1.0f);
-	output.Nor = float4(normalize(input.Nor), 1.0f);
-	output.Dif = shaderTexture.Sample(sampleState,input.Tex);
-	//output.Dif = float4(1.0f,0.0f,0.0f,1.0f);
-	output.Spec = float4(0.0f, 0.0f, 0.0f, 0.0f);	// Change in the future
+	float4 Spec = float4(0.0f,1.0f,0.0f,0.0f);
+
+	output.Pos = float4(input.WPos, Spec.x);
+	output.Nor = float4(normalize(input.Nor), Spec.y);
+	output.Dif = float4(shaderTexture.Sample(sampleState,input.Tex).xyz, Spec.z);
 
 	return output;
 }
