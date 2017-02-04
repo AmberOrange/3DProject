07047b1f19b3bad35ae28b3cde5f3618b49e3a71
Emil <emilgrenebrant@hotmail.com>
2017-02-04 15:04:01 +0100

    Reverted to previous input

---
+++ New3DProject/Timer.hpp
@@ -0,0 +1,28 @@
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <d3d11.h>
+
+class Timer
+{
+public:
+	Timer();
+	Timer(const Timer &other);
+	virtual ~Timer();
+
+	void StartTimer();
+	double GetTime();
+	double GetFrameTime();
+
+private:
+	double countsPerSecond;
+	__int64 CounterStart;
+
+	int frameCount;
+	int fps;
+
+	__int64 frameTimeOld;
+	double frameTime;
+}
+
+#endif
